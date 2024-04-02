1382d03f2a2e687a4926cc64356767d36519aac0
Casper Guo <casperg@umich.edu>
2024-04-02 17:13:17 -0400

    Fix Python file path

--- .github/workflows/edit_readme.py
+++ .github/workflows/edit_readme.py
@@ -3,4 +3,5 @@ import os
 
 if __name__ == "__main__":
     issue_content = json.loads(os.environ.get('ISSUE_CONTENT', '""'))
+    print([f for f in os.listdir(".") if os.path.isfile(f)])
     print(issue_content)
