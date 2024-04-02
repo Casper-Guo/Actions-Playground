869605e94216c1712cf3c078c2bb34adf1ff407b
Casper Guo <casperg@umich.edu>
2024-04-02 18:28:08 -0400

    Add edit README script

--- .github/workflows/edit_readme.py
+++ .github/workflows/edit_readme.py
@@ -1,7 +1,43 @@
 import json
+import re
 import os
 
+
+def parse_response(response: str) -> tuple[str]:
+    """Separate the individual responses in the full response body."""
+    # filter out potential empty string in the first split
+    fields = [field for field in response.split("###") if field]
+
+    # fetch the responses in each field
+    # see README for assumptions about JSON formatting
+    return tuple(re.search(r"\n+\s*(.*)\s*$", field.rstrip()).group(1)
+                 for field in fields)
+
+
+def shorten_url(url: str) -> str:
+    "Remove query strings from submitted URLs."
+    # Capture the url starting at www until the first question mark
+    return re.search(r"^(http[s]?:\/\/)?([^\s\?]*)\?*", url).group(2)
+
+
+def format_addition(response: tuple[str]) -> str:
+    """Format issue response to a line in README.
+
+    Change if README format and form questions drifts from assumptions listed in README
+    and update README accordingly.
+    """
+    company, career_url, pos, pos_url, location, degree, note = response
+    career_url = shorten_url(career_url)
+    pos_url = shorten_url(pos_url)
+    return f"| [{company}]({career_url}) | [{pos}]({pos_url}) | {location} | {degree} | {note if note != '_No response_' else ''}".strip()
+
+
 if __name__ == "__main__":
-    issue_content = json.loads(os.environ.get('ISSUE_CONTENT', '""'))
-    print([f for f in os.listdir(".") if os.path.isfile(f)])
-    print(issue_content)
+    form_body = json.loads(os.environ.get('ISSUE_CONTENT', '""'))["body"]
+    # with open(".github/workflows/test.json", "r") as f:
+    #     form_body = json.load(f)["body"]
+    response = parse_response(form_body)
+
+    with open("README.md", "a") as readme:
+        readme.write("\n")
+        readme.write(format_addition(response))
---
+++ README.md
@@ -0,0 +1,6 @@
+Welcome to the 2025 data science internship repo by Michigan Data Science Team!
+
+| Company | Role | Location | Min. Degree | Notes |
+| ------- | ---- | -------- | ----------- | ----- |
+| Visa | [	Associate Data Scientist – Intern - Cybersecurity](https://jobs.smartrecruiters.com/Visa/743999943242512) | Ashburn, VA | Bachelors |
+| Vestmark | [Information Systems Intern](https://simplify.jobs/c/Vestmark) | Wakefield, MA | Bachelors |
